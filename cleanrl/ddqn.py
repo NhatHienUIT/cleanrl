86ed0aa0da79d95cf5f2d7d7e784fbb2c0b4d0b9
NhatHienUIT <162800174+NhatHienUIT@users.noreply.github.com>
2024-12-12 22:25:57 +0700

    Update ddqn.py

--- cleanrl/ddqn.py
+++ cleanrl/ddqn.py
@@ -75,7 +75,7 @@ def make_env(env_id, seed, idx, capture_video, run_name):
     def thunk():
         if capture_video and idx == 0:
             env = gym.make(env_id, render_mode="rgb_array")
-            env = gym.wrappers.RecordVideo(env, f"videos/{run_name}", step_trigger = 100000)
+            env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
         else:
             env = gym.make(env_id)
         env = gym.wrappers.RecordEpisodeStatistics(env)
